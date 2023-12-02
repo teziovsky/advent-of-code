6b3d2c5e8270fd23f394530fed255dbc094c7a50
Jakub Soboczyński <teziovsky@gmail.com>
2023-12-02 09:28:18 +0100

    chore: update template with loop each line

--- 2023/template/index.ts
+++ 2023/template/index.ts
@@ -2,8 +2,17 @@ const file = Bun.file(import.meta.dir + "/input.txt");
 
 export const input = await file.text();
 
+const splitted = input.split("\n");
+
 function ex1() {
-  console.log("input:", input);
+  let result = 0;
+
+  splitted.forEach((line) => {
+    if (!line) return;
+    console.log("line:", line);
+  });
+
+  console.log("EX1 result: ", result);
 }
 
 ex1();
@@ -11,7 +20,14 @@ ex1();
 console.log("\n");
 
 function ex2() {
-  console.log("input:", input);
+  let result = 0;
+
+  splitted.forEach((line) => {
+    if (!line) return;
+    console.log("line:", line);
+  });
+
+  console.log("EX2 result: ", result);
 }
 
 ex2();
