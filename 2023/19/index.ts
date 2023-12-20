261ce55d84203272d81a8b83eddf8e6c403baa52
Jakub Soboczyński <teziovsky@gmail.com>
2023-12-20 16:12:16 +0100

    feat: solutions 2023-12-19 part2

--- 2023/19/index.ts
+++ 2023/19/index.ts
@@ -11,15 +11,16 @@ async function ex1(path: Path, delimiter = "\n") {
       const steps = rules.split(",").map((step) => {
         const [condition, target] = step.split(":");
         if (!target) return { condition: () => true, target: condition };
-        if (condition[1] === "<") {
-          const [field, value] = condition.split("<");
-          const numberedValue = Number(value);
-          return { condition: (obj: Record<string, number>) => obj[field] < numberedValue, target };
-        } else if (condition[1] === ">") {
-          const [field, value] = condition.split(">");
-          const numberedValue = Number(value);
-          return { condition: (obj: Record<string, number>) => obj[field] > numberedValue, target };
-        }
+        const operator = condition[1];
+        const [field, value] = condition.split(operator);
+        const numberedValue = Number(value);
+        return {
+          condition: (obj: Record<string, number>) => {
+            if (operator === "<") return obj[field] < numberedValue;
+            if (operator === ">") return obj[field] > numberedValue;
+          },
+          target,
+        };
       });
 
       return { ...obj, [name]: steps };
@@ -45,16 +46,11 @@ async function ex1(path: Path, delimiter = "\n") {
 
     while (currentTarget !== "A" && currentTarget !== "R") {
       const workflow = workflowsMap[currentTarget];
-      let index = 0;
-      while (true) {
-        const step = workflow[index];
-        const result = step.condition(rating);
-        if (result) {
-          currentTarget = step.target;
-          break;
-        } else {
-          index++;
-        }
+      const step = workflow.find((step) => step.condition?.(rating));
+      if (step) {
+        currentTarget = step.target;
+      } else {
+        currentTarget = "R";
       }
     }
 
@@ -71,7 +67,7 @@ async function ex2(path: Path, delimiter = "\n") {
 
   const workflowsMap: Record<
     string,
-    { condition: { operator: string; field: string; value: number }; target: string }[]
+    { condition?: { operator: string; field: string; value: number }; target: string }[]
   > = workflowsRows
     .split("\n")
     .filter(Boolean)
@@ -80,15 +76,10 @@ async function ex2(path: Path, delimiter = "\n") {
       const steps = rules.split(",").map((step) => {
         const [condition, target] = step.split(":");
         if (!target) return { target: condition };
-        if (condition[1] === "<") {
-          const [field, value] = condition.split("<");
-          const numberedValue = Number(value);
-          return { condition: { operator: "<", field, value: numberedValue }, target };
-        } else if (condition[1] === ">") {
-          const [field, value] = condition.split(">");
-          const numberedValue = Number(value);
-          return { condition: { operator: ">", field, value: numberedValue }, target };
-        }
+        const operator = condition[1];
+        const [field, value] = condition.split(operator);
+        const numberedValue = Number(value);
+        return { condition: { operator, field, value: numberedValue }, target };
       });
 
       return { ...obj, [name]: steps };
@@ -96,13 +87,51 @@ async function ex2(path: Path, delimiter = "\n") {
 
   let result = 0;
 
-  for (const rules of Object.values(workflowsMap)) {
-    const ranges: Record<string, number[]> = {
-      x: Array(4000).fill(1),
-      m: Array(4000).fill(1),
-      a: Array(4000).fill(1),
-      s: Array(4000).fill(1),
-    };
+  const ranges: { condition: Record<string, { min: number; max: number }>; target: string }[] = [
+    {
+      condition: {
+        x: { min: 1, max: 4000 },
+        m: { min: 1, max: 4000 },
+        a: { min: 1, max: 4000 },
+        s: { min: 1, max: 4000 },
+      },
+      target: "in",
+    },
+  ];
+
+  while (ranges.length) {
+    const current = ranges.pop()!;
+
+    if (current.target === "A") {
+      const { x, m, a, s } = current.condition;
+      result += (x.max - x.min + 1) * (m.max - m.min + 1) * (a.max - a.min + 1) * (s.max - s.min + 1);
+    } else if (current.target !== "R") {
+      const workflow = workflowsMap[current.target];
+
+      for (const step of workflow) {
+        if (step.condition) {
+          const {
+            condition: { operator, field, value },
+            target,
+          } = step;
+          const clonedRange = structuredClone(current);
+          Object.assign(clonedRange, { target: target });
+
+          if (operator === "<" && current.condition[field].min < value - 1) {
+            clonedRange.condition[field].max = value - 1;
+            current.condition[field].min = value;
+            ranges.push(clonedRange);
+          } else if (operator === ">" && current.condition[field].max > value + 1) {
+            clonedRange.condition[field].min = value + 1;
+            current.condition[field].max = value;
+            ranges.push(clonedRange);
+          }
+        } else {
+          current.target = step.target;
+          ranges.push(current);
+        }
+      }
+    }
   }
 
   return result;
@@ -113,5 +142,5 @@ console.log("EX1 Test Result: ", await ex1("19/test1", "\n\n"));
 console.log("EX1 Input Result: ", await ex1("19/input", "\n\n"));
 console.log("-----------------------");
 console.log("EX2 Test Result: ", await ex2("19/test2", "\n\n"));
-// console.log("EX2 Result: ", await ex2("19/input", "\n\n"));
+console.log("EX2 Result: ", await ex2("19/input", "\n\n"));
 console.log("-----------------------");
