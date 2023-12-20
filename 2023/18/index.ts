18f5590bfc21ec304bed3113d9f254d3e79ea5fc
Jakub Soboczyński <teziovsky@gmail.com>
2023-12-20 11:27:41 +0100

    feat: 2023-12-18 solution part2

--- 2023/18/index.ts
+++ 2023/18/index.ts
@@ -1,5 +1,4 @@
 import { type Path, loadFile } from "../utils";
-// part 2: 127844509405501
 
 type DIRECTION = [number, number];
 
@@ -59,49 +58,55 @@ async function ex2(path: Path, delimiter = "\n") {
 
   let currentY = 0;
   let currentX = 0;
-  const visited = new Set<string>();
-  visited.add(`${currentY},${currentX}`);
+
+  let area = 1;
 
   for (const line of rows) {
-    const [direction, meters] = line.split(" ");
-    const numberedMeters = Number(meters);
+    const { direction, number } = parseLine(line);
 
     const dir = DIRECTIONS[direction];
 
-    for (let i = 0; i < numberedMeters; i++) {
+    const lastY = currentY;
+    const lastX = currentX;
+
+    for (let i = 0; i < number; i++) {
       currentY += dir[0];
       currentX += dir[1];
-      visited.add(`${currentY},${currentX}`);
     }
+
+    area += (currentY * lastX - currentX * lastY + number) / 2;
   }
 
-  const queue = [[1, 1]];
-
-  while (queue.length) {
-    const [y, x] = queue.pop()!;
-
-    const key = `${y},${x}`;
-
-    if (!visited.has(key)) {
-      visited.add(key);
-
-      for (const direction in DIRECTIONS) {
-        const dir = DIRECTIONS[direction];
-        const next = [y + dir[0], x + dir[1]];
-        if (!visited.has(`${next[0]},${next[1]}`)) {
-          queue.push(next);
-        }
-      }
-    }
-  }
-
-  return visited.size;
+  return area;
 }
 
 console.log("-----------------------");
 console.log("EX1 Test Result: ", await ex("18/test1"));
 console.log("EX1 Input Result: ", await ex("18/input"));
 console.log("-----------------------");
-// console.log("EX2 Test Result: ", await ex2("18/test2"));
-// console.log("EX2 Result: ", await ex2("18/input"));
+console.log("EX2 Test Result: ", await ex2("18/test2"));
+console.log("EX2 Result: ", await ex2("18/input"));
 console.log("-----------------------");
+
+function parseLine(line: string) {
+  const [value] = line.match(/[0-9a-f]{6}/)!;
+  const direction = value[5];
+  const number = parseInt(value.substring(0, 5), 16);
+
+  return { number, direction: matchDirection(direction) };
+}
+
+function matchDirection(int: string) {
+  switch (int) {
+    case "0":
+      return "R";
+    case "1":
+      return "D";
+    case "2":
+      return "L";
+    case "3":
+      return "U";
+    default:
+      return "R";
+  }
+}
