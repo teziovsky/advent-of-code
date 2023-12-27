1276b812b2e5aa6b055c3a3e09a5259f80841be8
Jakub Soboczyński <teziovsky@gmail.com>
2023-12-27 16:53:51 +0100

    feat: solutions 2023-12-23

--- 2023/23/index.ts
+++ 2023/23/index.ts
@@ -1,8 +1,6 @@
 import { type Path, loadFile } from "../../utils";
 
-type DIRECTION = [number, number];
-
-const DIRECTIONS: Record<string, DIRECTION> = {
+const DIRECTIONS: Record<string, [number, number]> = {
   "^": [-1, 0],
   ">": [0, 1],
   v: [1, 0],
@@ -12,111 +10,121 @@ const DIRECTIONS: Record<string, DIRECTION> = {
 async function ex1(path: Path, delimiter = "\n") {
   const rows = (await loadFile(path, delimiter)).filter(Boolean).map((row) => row.split(""));
 
-  const startingPoint = [0, 1];
+  let result = 0;
+  const initialPosition: number[] = [0, 1];
 
-  return rows.reduce((maxLength) => {
-    const queue = [[startingPoint.join()]];
+  const queue = [[initialPosition.join()]];
 
-    while (queue.length) {
-      const path = queue.shift()!;
-      const [y, x] = path.at(-1)!.split(",").map(Number);
-      const char = rows[y][x];
+  while (queue.length) {
+    const coords = queue.shift()!;
+    const [y, x] = coords.at(-1)!.split(",").map(Number);
+    const char = rows[y][x];
 
-      for (const dir in DIRECTIONS) {
-        if (char in DIRECTIONS && char !== dir) continue;
+    for (const dir in DIRECTIONS) {
+      if (char in DIRECTIONS && char !== dir) continue;
 
-        const [directionY, directionX] = DIRECTIONS[dir];
-        const nextY = y + directionY;
-        const nextX = x + directionX;
-        const nextNode = [nextY, nextX].join();
-        if (path.includes(nextNode)) continue;
-        switch (rows[nextY]?.[nextX]) {
-          case ".":
-          case "^":
-          case ">":
-          case "v":
-          case "<":
-            queue.push(path.concat(nextNode));
-        }
-      }
+      const [directionY, directionX] = DIRECTIONS[dir];
+      const nextY = y + directionY;
+      const nextX = x + directionX;
+      const nextPosition = [nextY, nextX].join();
+      if (coords.includes(nextPosition)) continue;
 
-      if (y === rows.length - 1) {
-        maxLength = Math.max(maxLength, path.length - 1);
+      if ([".", "^", ">", "v", "<"].includes(rows[nextY]?.[nextX])) {
+        queue.push(coords.concat(nextPosition));
       }
     }
 
-    return maxLength;
-  }, 0);
+    if (y === rows.length - 1) {
+      result = Math.max(result, coords.length - 1);
+    }
+  }
+
+  return result;
 }
 
+type Distances = Record<string, Record<string, number>>;
+
 async function ex2(path: Path, delimiter = "\n") {
-  const rows = (await loadFile(path, delimiter)).filter(Boolean).map((row) => {
-    return row
-      .split("")
-      .filter(Boolean)
-      .map((char) => {
-        switch (char) {
-          case "^":
-          case ">":
-          case "v":
-          case "<":
-            return ".";
-        }
-        return char;
-      });
-  });
+  const rows = await createGraph(path, delimiter);
 
-  const start = [0, 1].join();
-  const end = [rows.length - 1, rows[0].length - 2].join();
-  const nodes = [start, end];
-  const distances = [];
-  for (const node of nodes) {
-    distances[node] = {};
-    const [r, c] = node.split(",").map(Number);
+  const initialPosition = [0, 1].join();
+  const finalPosition = [rows.length - 1, rows[0].length - 2].join();
+  const positions = [initialPosition, finalPosition];
 
-    function move(nSteps, prevDir, r, c) {
-      if (rows[r]?.[c] !== ".") return;
+  const distances = calculateDistances(positions, rows);
 
-      const nEdges = Object.values(dirs).filter(([dr, dc]) => rows[r + dr]?.[c + dc] === ".").length;
-      if (nSteps && (nEdges > 2 || !r || r === rows.length - 1)) {
-        const nextNode = [r, c].join();
-        if (!nodes.includes(nextNode)) {
-          nodes.push(nextNode);
-        }
-        distances[node][nextNode] = nSteps;
-        return;
-      }
-
-      if (prevDir !== "v" && r) move(nSteps + 1, "^", r - 1, c);
-      if (prevDir !== "^" && r !== rows.length - 1) move(nSteps + 1, "v", r + 1, c);
-      if (prevDir !== ">") move(nSteps + 1, "<", r, c - 1);
-      if (prevDir !== "<") move(nSteps + 1, ">", r, c + 1);
-    }
-
-    move(0, null, r, c);
-  }
-
-  let maxSteps = 0;
-  function move(steps, node, path) {
-    if (node === end) {
-      maxSteps = Math.max(maxSteps, steps);
-      return;
-    }
-    path.push(node);
-    for (const nextNode in distances[node]) {
-      if (path.includes(nextNode)) continue;
-      move(steps + distances[node][nextNode], nextNode, [...path]);
-    }
-  }
-
-  move(0, start, []);
-  console.log(maxSteps);
+  return findLongestPath(finalPosition, distances, initialPosition);
 }
 
 console.log("-----------------------");
 console.log("EX1 Test Result: ", await ex1("23/test1"));
 console.log("EX1 Input Result: ", await ex1("23/input"));
 console.log("-----------------------");
-// console.log("EX2 Test Result: ", await ex2("23/test2"));
-// console.log("EX2 Result: ", await ex2("23/input"));
+console.log("EX2 Test Result: ", await ex2("23/test2"));
+console.log("EX2 Result: ", await ex2("23/input"));
 console.log("-----------------------");
+
+async function createGraph(path: Path, delimiter: string) {
+  return (await loadFile(path, delimiter))
+    .filter(Boolean)
+    .map((row) => row.split("").map((char) => (["^", ">", "v", "<"].includes(char) ? "." : char)));
+}
+
+function calculateDistances(positions: string[], rows: string[][]) {
+  const distances: Distances = {};
+
+  for (const position of positions) {
+    distances[position] = {};
+
+    const [y, x] = position.split(",").map(Number);
+
+    function move(stepsCount: number, lastDirection: string | null, y: number, x: number): void {
+      if (rows[y]?.[x] !== ".") return;
+
+      const edgesCount = Object.values(DIRECTIONS).filter(
+        ([directionY, directionX]) => rows[y + directionY]?.[x + directionX] === "."
+      ).length;
+
+      if (stepsCount && (edgesCount > 2 || !y || y === rows.length - 1)) {
+        const nextPosition = [y, x].join();
+
+        if (!positions.includes(nextPosition)) {
+          positions.push(nextPosition);
+        }
+
+        distances[position][nextPosition] = stepsCount;
+        return;
+      }
+
+      if (lastDirection !== "v" && y) move(stepsCount + 1, "^", y - 1, x);
+      if (lastDirection !== "^" && y !== rows.length - 1) move(stepsCount + 1, "v", y + 1, x);
+      if (lastDirection !== ">") move(stepsCount + 1, "<", y, x - 1);
+      if (lastDirection !== "<") move(stepsCount + 1, ">", y, x + 1);
+    }
+
+    move(0, null, y, x);
+  }
+  return distances;
+}
+
+function findLongestPath(finalPosition: string, distances: Distances, initialPosition: string) {
+  let result = 0;
+
+  function dfs(steps: number, currentPosition: string, visitedPositions: string[]) {
+    if (currentPosition === finalPosition) {
+      result = Math.max(result, steps);
+      return;
+    }
+
+    visitedPositions.push(currentPosition);
+
+    for (const nextPosition in distances[currentPosition]) {
+      if (visitedPositions.includes(nextPosition)) continue;
+      dfs(steps + distances[currentPosition][nextPosition], nextPosition, [...visitedPositions]);
+    }
+  }
+
+  dfs(0, initialPosition, []);
+
+  return result;
+}
